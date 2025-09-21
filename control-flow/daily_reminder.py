08de8ca31a02348d5873dcd392a3d6ee20c83b1a
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-09-21 21:01:51 +0100

    implement last task

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -3,8 +3,21 @@ priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no)")
 
 match priority:
-    case high:
+    case "high":
         if time_bound == "yes":
-            print("You have to do this as before the schedule")
+            print(f"'{task}' is a high priority task that requires immediate attention today!")
         else:
-            print("You need to do this as soon as possible")
+            print(f"'{task}' is a high priority task that requires attention soon!")
+
+    case "medium":
+        if time_bound == "yes":
+            print(f"'{task}' is a medium priority task that requires attention today!")
+        else:
+            print(f"'{task}' is a medium priority task that requires attention soon!")
+    case "low":
+        if time_bound == "no":
+            print(f"'{task}'is a low priority task. Consider completing it when you have free time. ")
+        else:
+            print(f"'{task}'is a low priority task. Consider completing it when anytime today.")
+    case _:
+        print(f"Enter the right priority")
