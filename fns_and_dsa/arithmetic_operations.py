43fbbc387c0db7779ae062bbd93a4ac661d6614d
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-09-25 22:49:15 +0100

    checks for divide by zero

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -5,5 +5,7 @@ def perform_operation(num1, num2, operation):
         return num1 - num2
     elif operation == "multiply":
         return num1 * num2
-    else:
+    elif operation == "division":
+        if num2 == 0:
+            print(f"Division of number by zero is not allowed")
         return num1 / num2
