83dab1f8f76116021629cc38ed6443ccfa64290e
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-09-27 14:36:23 +0100

    implement task 3

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,20 @@
+def display_current_datetime() :
+    import datetime
+    current_date = datetime.datetime.now()
+
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(formatted_date)
+
+def display_future_datetime():
+    from datetime import timedelta, datetime
+
+    number_of_days = int(input("Enter your number of days: "))
+    current_date = datetime.now()
+
+    future_date  = current_date + timedelta(days=number_of_days)
+    formatted_date = future_date.strftime("%Y-%m-%d")
+
+    print(formatted_date)
+
+display_current_datetime()
+display_future_datetime()
