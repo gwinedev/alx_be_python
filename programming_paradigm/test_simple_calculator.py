29cbc55f310d325fd00537300921be514f9fd691
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-10-04 18:31:35 +0100

    task on testing done

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,34 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 5), 5)
+
+    def test_subtraction(self):
+        """Test the subtraction method."""
+        self.assertEqual(self.calc.subtract(10, 4), 6)
+        self.assertEqual(self.calc.subtract(4, 10), -6)
+        self.assertEqual(self.calc.subtract(10, 0), 10)
+
+    def test_multiplication(self):
+        """Test the multiplication method"""
+        self.assertEqual(self.calc.multiply(2,3), 6)
+        self.assertEqual(self.calc.multiply(2,-3), -6)
+        self.assertEqual(self.calc.multiply(-2,-3), 6)
+        self.assertEqual(self.calc.multiply(2,0), 0)
+
+    def test_division(self):
+        """Test the division method"""
+        self.assertEqual(self.calc.divide(15, 3), 5)
+        self.assertEqual(self.calc.divide(15, -3), -5)
+        self.assertEqual(self.calc.divide(-15, -3), 5)
+# Remember to write additional test methods for subtract, multiply, and divide.
