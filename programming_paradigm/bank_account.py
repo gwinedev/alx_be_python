52b3efa0ddf2a693eec96d997cecdd042fdc3c0c
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-10-04 12:31:30 +0100

    implement task 1

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,12 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        self.account_balance -= amount
+
+    def display_balance(self):
+        print(f' Current Balance: ${self.account_balance}')
